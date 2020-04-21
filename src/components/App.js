2065d9438ff437bca3ff013fee4129703d2de6a4
Alex Love <lazd53@gmail.com>
2020-04-20 19:50:33 -0700

    Create BtpMain component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 
 import BtpHeader from './BtpHeader';
+import BtpMain from './BtpMain';
 
 function App() {
   return (
     <div className="App">
       <BtpHeader />
+      <BtpMain />
     </div>
   );
 }
---
+++ src/components/SocialMediaButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SocialMediaButton({text}){
+  return (
+    <button className="social-media-button">
+      {text}
+    </button>
+  )
+}
