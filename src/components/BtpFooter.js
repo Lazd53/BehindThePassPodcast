1a94d5c4404d141425d6ad8a8f62a074ba0ab8ba
Alex Love <lazd53@gmail.com>
2020-04-21 12:06:16 -0700

    Add footer - incomplete

---
+++ src/components/BtpFooter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BtpFooter extends React.Component{
+  render(){
+    return(
+      <footer className="footer-play-bar">
+        <h3 className="current-episode-title"> Episode 11: The Named Episode </h3>
+        <button className="play-button"> Play button </button>
+      </footer>
+    )
+  }
+}
+
+export default BtpFooter
