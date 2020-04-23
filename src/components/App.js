21032a57aa89184e9bf890cbf3c8e1d57169c7ab
Alex Love <lazd53@gmail.com>
2020-04-23 16:03:09 -0700

    Rebuild app to use Grid to contain primary pieces.
    BtpMain converted to PodcastDescription, and dropped in <main> container 
    with Episodes component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.scss';
 
 import BtpHeader from './BtpHeader';
-import BtpMain from './BtpMain';
+import PodcastDescription from './PodcastDescription';
 import Episodes from './Episodes';
+import BtpFooter from './BtpFooter';
 
 function App() {
   return (
     <div className="App">
       <BtpHeader />
-      <BtpMain />
-      <Episodes />
+        <main>
+          <PodcastDescription />
+          <Episodes />
+        </main>
+      <BtpFooter/>
     </div>
   );
 }
--- src/components/PodcastDescription.js
+++ src/components/PodcastDescription.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SocialMediaButton from './SocialMediaButton';
 
-export default function BtpMain (props) {
+export default function PodcastDescription (props) {
   return (
-    <main>
+    <section className="podcast-description">
       <p>Listen up! - a Sacramento based group of friends who talk sports, food, and non-sense. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <div className="social-media-btn-container">
         <SocialMediaButton text="Add us on Apple Music"/>
         <SocialMediaButton text="Follow us on Twitter"/>
       </div>
-    </main>
+    </section>
   )
 }
