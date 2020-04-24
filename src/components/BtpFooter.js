90bae4b23843801397397084a23671943dd4f88f
Alex Love <lazd53@gmail.com>
2020-04-23 17:14:18 -0700

    Style play button to have play/pause shape

--- src/components/BtpFooter.js
+++ src/components/BtpFooter.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 class BtpFooter extends React.Component{
+  state = {playing: true}
   render(){
     return(
       <footer className="footer-play-bar">
         <h3 className="current-episode-title"> Episode 11: The Named Episode </h3>
-        <button className="play-button"> Play button </button>
+        <button
+          className={this.state.playing?"play-button pause":"play-button play"}
+          onClick={()=> {
+            this.state.playing
+              ? this.setState({playing: false})
+              : this.setState({playing: true})
+              }
+            }
+        >
+          Play button
+        </button>
       </footer>
     )
   }
