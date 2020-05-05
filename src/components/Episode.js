c99b70ab3e16fe97246da257a37f274366f99a3e
Alex Love <lazd53@gmail.com>
2020-05-05 14:26:46 -0700

    Allow click to select episode - logic only

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -3,10 +3,25 @@ import {calcRunTime} from '../utils/helpers'
 
 class Episode extends React.Component{
 
+  clickHandler = ( event ) =>{
+    const {podcast, handleEpisodeClick} = this.props
+    handleEpisodeClick(podcast.id)
+  }
+
+  buildArticleClassName = () =>{
+    const {isSelected, isPlaying} = this.props
+    let className = "single-episode";
+    if ( isSelected ){ className += " selected-now" }
+    if ( isPlaying ){ className += " playing-now" }
+    return className
+  }
+
   render(){
     const {id, episodeNo, name, runTime, airDate} = this.props.podcast;
+    const {handleEpisodeClick, currentEpisode, isSelected, isPlaying} = this.props
+
     return(
-      <article className="single-episode">
+      <article className={this.buildArticleClassName()}>
         <div className="episode-title-line">
           <h3 className="episode-title">Episode {episodeNo}: {name}</h3>
           <p className="episode-runtime">{calcRunTime(runTime)}</p>
@@ -14,6 +29,7 @@ class Episode extends React.Component{
         </div>
         <div className="episode-description">
         </div>
+        <button onClick={this.clickHandler}>TestButton</button>
       </article>
     )
   }
--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,15 +3,36 @@ import {connect} from "react-redux"
 import Episode from './Episode'
 
 class Episodes extends React.Component{
+  state = { seeEpisodeDetails: false }
+
+  componentDidUpdate(){
+    console.log(this.state.seeEpisodeDetails)
+  }
+  handleEpisodeClick = (episodeId) =>{
+    this.state.seeEpisodeDetails === false || this.state.seeEpisodeDetails !== episodeId
+      ? this.setState({seeEpisodeDetails: episodeId})
+      : this.setState({seeEpisodeDetails: false})
+    }
 
   createEpisodes = () => {
     const {podcasts, currentPod} = this.props;
     const podcastList = Object.values(podcasts)
-    return podcastList.map( podcast => <Episode podcast = {podcast} />)
+    return podcastList.map( podcast => {
+      const currentEpisode = false;
+      if (currentPod && currentPod.id === podcast.id){
+          currentEpisode = true
+      }
+      return (
+        <Episode key={podcast.id}
+          handleEpisodeClick={this.handleEpisodeClick}
+          currentEpisode={currentEpisode}
+          podcast = {podcast}
+          isSelected = {podcast.id === this.state.seeEpisodeDetails}
+          isPlaying = {true}
+        />)
+    })
 
   }
-
-
 
   render(){
     return(
@@ -27,7 +48,9 @@ class Episodes extends React.Component{
 
 const mapStatetoProps = ( store ) =>({
   podcasts: store.allPodcasts,
-  currentPod: store.allPodcasts[store.selectPodcast]
+  currentPod: store.selectedPodcast === ""
+                ? false
+                : store.allPodcasts[store.selectedPodcast]
 })
 
 export default connect(mapStatetoProps)(Episodes);
